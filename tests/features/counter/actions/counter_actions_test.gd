de02aec0533864e581b257093d0265af94b26476
Julio Cesar <82642883+jcmljunior@users.noreply.github.com>
2023-12-31 14:52:29 -0300

    correção nos mocks

--- tests/features/counter/actions/counter_actions_test.gd
+++ tests/features/counter/actions/counter_actions_test.gd
@@ -1,16 +1,54 @@
 extends GutTest
 
+class MockCounter:
+	
+	const INCREMENT_COUNTER := "INCREMENT_COUNTER"
+	const DECREMENT_COUNTER := "DECREMENT_COUNTER"
+	
+	var state := {
+		"counter": 10,
+	}
+
+	func increment_counter(state: int) -> Dictionary:
+		return {
+			"type": INCREMENT_COUNTER,
+			"payload": {
+				"counter": state,
+			}
+		}
+		
+	func decrement_counter(state: int) -> Dictionary:
+		return {
+			"type": DECREMENT_COUNTER,
+			"payload": {
+				"counter": state,
+			}
+		}
+	
+	func reducer(state: int, action: Dictionary) -> Dictionary:
+		match(action.get("type")):
+			INCREMENT_COUNTER:
+				return increment_counter(state+1)
+				
+			DECREMENT_COUNTER:
+				return decrement_counter(state-1)
+			
+			_:
+				return {
+					"type": "",
+					"payload": {
+						"counter": state,
+					}
+				}
+
 class TestCounterActions:
 	extends GutTest
 	
-	var mock_counter := {
-		"counter": 10,
-	}
+	var Mock = MockCounter.new()
 	
 	func test_increment_counter():
-		var response: Dictionary = CounterReducers.counter_reducer(
-			mock_counter.get("counter"),
-			CounterActions.increment_counter(mock_counter.get("counter")))
+		var response: Dictionary = Mock.reducer(Mock.get("state").get("counter"),
+			Mock.increment_counter(Mock.get("state").get("counter")))
 		
 		# Validações de parametros necessários para execução.
 		assert_has(response, "payload")
@@ -18,12 +56,11 @@ class TestCounterActions:
 		assert_has(response.get("payload"), "counter")
 		
 		# Validação da incrementação do contador.
-		assert_eq(response.get("payload").get("counter"), mock_counter.get("counter")+1)
+		assert_eq(response.get("payload").get("counter"), Mock.get("state").get("counter")+1)
 
 	func test_decrement_counter():
-		var response: Dictionary = CounterReducers.counter_reducer(
-			mock_counter.get("counter"),
-			CounterActions.decrement_counter(mock_counter.get("counter")))
+		var response: Dictionary = Mock.reducer(Mock.get("state").get("counter"),
+			Mock.decrement_counter(Mock.get("state").get("counter")))
 		
 		# Validações de parametros necessários para execução.
 		assert_has(response, "payload")
@@ -31,4 +68,4 @@ class TestCounterActions:
 		assert_has(response.get("payload"), "counter")
 		
 		# Validação da incrementação do contador.
-		assert_eq(response.get("payload").get("counter"), mock_counter.get("counter")-1)
+		assert_eq(response.get("payload").get("counter"), Mock.get("state").get("counter")-1)
--- tests/features/player/actions/player_actions_test.gd
+++ tests/features/player/actions/player_actions_test.gd
@@ -1,25 +1,59 @@
 extends GutTest
 
-class TestPlayerActions:
-	extends GutTest
-	
-	var mock_player_name = {
+class MockPlayer:
+	const PLAYER_SET_NAME := "PLAYER_SET_NAME"
+	const PLAYER_SET_AGE := "PLAYER_SET_AGE"
+	const state: Dictionary = {
 		"player": {
 			"name": "",
-		}
-	}
-	
-	var mock_player_age = {
-		"player": {
 			"age": 0,
 		}
 	}
 	
-	func test_player_set_name():
-		var response: Dictionary = PlayerReducers.player_reducer(
-			mock_player_name,
-			PlayerActions.player_set_name(mock_player_name.get("player").get("name")))
+	func player_set_name(name: String) -> Dictionary:
+		return {
+			"type": PLAYER_SET_NAME,
+			"payload": {
+				"player": {
+					"name": name,
+				}
+			}
+		}
+
+	func player_set_age(age: int) -> Dictionary:
+		return {
+			"type": PLAYER_SET_AGE,
+			"payload": {
+				"player": {
+					"age": age,
+				}
+			}
+		}
 		
+	func reducer(state: Dictionary, action: Dictionary) -> Dictionary:
+		match(action.get("type")):
+			PLAYER_SET_NAME:
+				return player_set_name(action.get("payload").get("player").get("name"))
+				
+			PLAYER_SET_AGE:
+				return player_set_age(action.get("payload").get("player").get("age"))
+				
+			_:
+				return {
+					"type": "",
+					"payload": state,
+				}
+
+class TestPlayerActions:
+	extends GutTest
+	
+	var Mock = MockPlayer.new()
+	var player_name := "Amanda"
+	var player_age := 18
+	
+	func test_player_set_name():
+		var response = Mock.reducer(Mock.get("state"), Mock.player_set_name(player_name))
+
 		# Validações de parametros necessários para execução.
 		assert_has(response, "payload")
 		assert_has(response, "type")
@@ -28,21 +62,18 @@ class TestPlayerActions:
 		
 		# Validação da resposta.
 		assert_typeof(response.get("payload").get("player").get("name"), TYPE_STRING)
-		assert_true(response.get("payload").get("player").get("name") in PlayerUtils.player_names)
+		assert_true(response.get("payload").get("player").get("name") == player_name)
 		
 	
 	func test_player_set_age():
-		var response: Dictionary = PlayerReducers.player_reducer(
-			mock_player_age,
-			PlayerActions.player_set_age(mock_player_age.get("player").get("age"))
-		)
+		var response = Mock.reducer(Mock.get("state"), Mock.player_set_age(player_age))
 		
-		# Validações de parametros necessários para execução.
+		## Validações de parametros necessários para execução.
 		assert_has(response, "payload")
 		assert_has(response, "type")
 		assert_has(response.get("payload"), "player")
 		assert_has(response.get("payload").get("player"), "age")
-		
-		# Validação da resposta.
+		#
+		## Validação da resposta.
 		assert_typeof(response.get("payload").get("player").get("age"), TYPE_INT)
-		assert_true(response.get("payload").get("player").get("age") > mock_player_age.get("player").get("age"))
+		assert_true(response.get("payload").get("player").get("age") > Mock.get("state").get("player").get("age"))
